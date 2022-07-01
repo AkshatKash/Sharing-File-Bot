fe8486cab33297d5e3f7b6c992cafb8640499f2d
Sᴜʀᴀᴊ <108281894+TG-SURAJ@users.noreply.github.com>
2022-07-01 11:49:12 +0530

    Create cbb.py

---
+++ plugins/cbb.py
@@ -0,0 +1,27 @@
+# (©)Codexbotz
+# Recode by @KingOf_univers
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import Bot
+from config import OWNER
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"<i><b>Aʙᴏᴜᴛ Tʜɪs ʙᴏᴛ:\n\n@{client.username} Is A Tᴇʟᴇɢʀᴀᴍ Bᴏᴛ Tᴏ Sᴛᴏʀᴇ Pᴏsᴛs Oʀ Fɪʟᴇs Tʜᴀᴛ Cᴀɴ Bᴇ Aᴄᴄᴇssᴇᴅ Vɪᴀ Sᴘᴇᴄɪᴀʟ Lɪɴᴋs.\n\n○ Cʀᴇᴀᴛᴏʀ : @{OWNER}\n○ Cʜᴀɴɴᴇʟ: @Movies4youbackup\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ: @Movies_4you</b></i>\n",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Cʟᴏsᴇ ✗", callback_data="close")]]
+            ),
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except BaseException:
+            pass
